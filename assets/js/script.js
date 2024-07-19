b3a43a180aa1465a0178196bd1218dceabf29551
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-07-19 13:44:00 +0000

    harry questions array moved out of quiz function so that they could be accessed by check answers function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,44 @@
+let harryQuestions = [
+    {
+        question: "Question 1",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        correct: "Choice 1",
+    },
+    {
+        question: "Question 2",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        correct: "Choice 2",
+    },
+    {
+        question: "Question 3",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        correct: "Choice 3",
+    },
+    {
+        question: "Question 4",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        correct: "Choice 4",
+    },
+    {
+        question: "Question 5",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        correct: "Choice 4",
+    },
+    {
+        question: "Question 6",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        correct: "Choice 4",
+    },
+    {
+        question: "Question 7",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        correct: "Choice 4",
+    },
+];
+
 function harryQuiz(){
     let harryQuizPage = document.getElementById('gameArea');
-    let harryQuestions = [
-        {
-            question: "Question 1",
-            choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            correct: 0
-        },
-        {
-            question: "Question 2",
-            choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            correct: 0
-        },
-        {
-            question: "Question 3",
-            choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            correct: 0
-        },
-        {
-            question: "Question 4",
-            choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            correct: 0
-        },
-        {
-            question: "Question 5",
-            choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            correct: 0
-        },
-        {
-            question: "Question 6",
-            choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            correct: 0,
-        },
-        {
-            question: "Question 7",
-            choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-            correct: 0,
-        },
-    ];
+    
     let harryHtmlStructure = `
         <div>
             <h3></h3>
@@ -48,22 +50,24 @@ function harryQuiz(){
                 <h3>${harryQuestions.question}</h3>
             </div>
             <div>
-                <button onclick="checkHarryAnswer();">${harryQuestions.choices[0]}</button>
-                <button onclick="checkHarryAnswer();">${harryQuestions.choices[1]}</button>
-                <button onclick="checkHarryAnswer();">${harryQuestions.choices[2]}</button>
-                <button onclick="checkHarryAnswer();">${harryQuestions.choices[3]}</button>       
+                <button onclick="checkHarryAnswer(0);">${harryQuestions.choices[0]}</button>
+                <button onclick="checkHarryAnswer(1);">${harryQuestions.choices[1]}</button>
+                <button onclick="checkHarryAnswer(2);">${harryQuestions.choices[2]}</button>
+                <button onclick="checkHarryAnswer(3);">${harryQuestions.choices[3]}</button>       
             </div>
-            <p id="feedback"></p>
+            <div id="feedback"></div>
         `;
+        
     harryHtmlStructure += harryHtmlContent;
-    }
+    };
     harryQuizPage.innerHTML = harryHtmlStructure;
 };
+
 
 function ronQuiz(){
 
 }
 
 function hermionQuiz(){
-    
+
 }
